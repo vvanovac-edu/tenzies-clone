fc6a43e8efb516f624e2a4cdd2405b092e312fe9
Vlado <vanovac.vlado@gmail.com>
2022-02-16 14:26:31 +0100

    using Die component in App (hardcoded)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import Die from './components/Die/Die';
 
 export default function App() {
   return (
-    <h1>Tenzies Clone App!</h1>
+    <main>
+      <div className="dice-container">
+        <Die value="1"/>
+        <Die value="2"/>
+        <Die value="3"/>
+        <Die value="4"/>
+        <Die value="5"/>
+        <Die value="6"/>
+        <Die value="1"/>
+        <Die value="2"/>
+        <Die value="3"/>
+        <Die value="4"/>
+      </div>
+    </main>
   );
 }
