ecca84c4ee8a99acc85c29ff4fac860760a20dd5
Vlado <vanovac.vlado@gmail.com>
2022-02-16 15:57:28 +0100

    adding logic for finishing game

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti';
 
 import './App.css';
 import Die from './components/Die/Die';
 
 export default function App() {
+  const [tenzies, setTenzies] = useState(false);
+
   const generateDie = () => {
     const randomNumber = Math.ceil(Math.random() * 6);
 
@@ -16,7 +19,6 @@ export default function App() {
   }
 
   const allNewDice = () => {
-
     const newDice = [];
     for (let i = 0; i < 10; i++) {
       newDice.push(generateDie());
@@ -25,6 +27,17 @@ export default function App() {
   }
 
   const [dice, setDice] = useState(allNewDice())
+
+  useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((die) => die.value === firstValue);
+
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+    }
+  }, [dice]);
 
   const holdDice = (id) => {
     setDice((prevDice) => {
@@ -46,15 +59,25 @@ export default function App() {
   ))
 
   const rollDice = () => {
-    setDice((prevDice) => {
-      return prevDice.map((die) => {
-        return die.isHeld ? die : generateDie();
+    if (!tenzies) {
+      setDice((prevDice) => {
+        return prevDice.map((die) => {
+          return die.isHeld ? die : generateDie();
+        });
       });
-    });
+    } else {
+      setTenzies(false);
+      setDice(allNewDice())
+    }
   }
 
   return (
     <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+      </p>
       <div className="dice-container">
         {diceElements}
       </div>
@@ -62,7 +85,7 @@ export default function App() {
           className="roll-button"
           onClick={rollDice}
       >
-        Roll
+        {tenzies ? 'New Game' : 'Roll'}
       </button>
     </main>
   );
